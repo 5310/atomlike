54d0c76d6c414f1bc96ce682c2fe0e65391454ee
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2013-07-24 16:07:48 +0530

    PolyK component in.

---
+++ game/components/polyk.js
@@ -0,0 +1,68 @@
+Crafty.c('PolyK', {
+	
+	polyk_polygon: undefined,
+	polyk_auto: undefined,
+	
+	init: function() {
+		
+		this.requires('2D');
+		
+		this.polyk_polygon = [];
+		this.polyk_auto = true;
+		this.polyk_setPolygon();
+		
+	},
+	
+	polyk_setPolygon: function( polygon ) {
+		
+		if ( polygon ) {
+			
+			this.polyk_polygon = polygon;
+			this.polyk_auto = false;
+			
+		} else {
+			
+			this.polyk_auto = true;
+			this.polyk_polygon = [
+				-this.w/2, -this.h/2, 
+				-this.w/2, this.h/2, 
+				this.w/2, this.h/2, 
+				this.w/2, -this.h/2
+			];
+			
+		}
+	},
+	
+	polyk_transformedPolygon: function() {
+		
+		if ( this.poly_auto ) this.polyk_setPolygon();
+		
+		var polygon = Crafty.clone(this.polyk_polygon);
+		
+		var s = Math.sin(Math.toRad(this.rotation));
+		var c = Math.cos(Math.toRad(this.rotation));
+		for ( var i = 0; i < polygon.length; i += 2 ) {
+			var x = polygon[i] * c - polygon[i+1] * s;
+			var y = polygon[i] * s + polygon[i+1] * c;
+			x += this.x;
+			y += this.y;
+			polygon[i] = x;
+			polygon[i+1] = y;
+		}
+		
+		return polygon;
+	},
+	
+	polyk_containsPoint: function( x, y ) {		
+		return PolyK.ContainsPoint( this.polyk_transformedPolygon(), x, y );	
+	},
+	
+	polyk_rayCast: function( x, y, dx, dy ) {
+		return PolyK.RayCast( this.polyk_transformedPolygon(), x, y, dx, dy );		
+	},
+	
+	polyk_closestEdge: function( x, y ) {
+		return PolyK.ClosestEdge( this.polyk_transformedPolygon(), x, y );
+	},
+	
+});
--- game/main.js
+++ game/main.js
@@ -1,25 +1,36 @@
 window.onload = function() {
 	
-	// Start Crafty.
-	Crafty.init(800, 480);
-	
-	// Start Pixi.
-	Crafty.pixi.init(800, 480);
-	
-	e = Crafty.e("2D, PIXI").attr({x: 100, y: 100, w: 100, h: 50});
-	e.pixi_setGraphics();
-	e.pixi_setInteractive( true, true );
-	e.pixi_setHitArea();
-	
-	/*graphics = new PIXI.Graphics();
-	Crafty.pixi.container.addChild(graphics);
-	graphics.beginFill(0xAA0000);
-	graphics.drawCircle(100, 100, 200);
-	graphics.interactive = true;
-	graphics.hitArea = new PIXI.Rectangle(-100, -100, 200, 200);
-	graphics.drag = false;
-	graphics.mousedown = graphics.touchstart = function(data) {
-	    console.log(data);
-	};*/
+    // Start Crafty.
+    Crafty.init(800, 480);
+    
+    // Start Pixi.
+    Crafty.pixi.init(800, 480);
+    
+    // Tests.
+    e = Crafty.e("2D, PIXI, PolyK").attr({x: 100, y: 100, w: 100, h: 50, rotation: 30});
+    e.pixi_setGraphics();
+    e.pixi_setInteractive( true, true );
+    e.pixi_setHitArea();
+    e.polyk_setPolygon();
+    console.log(e.polyk_polygon);
+    console.log(e.polyk_transformedPolygon());
+    
+    /*graphics = new PIXI.Graphics();
+    Crafty.pixi.container.addChild(graphics);
+    graphics.beginFill(0xAA0000);
+    graphics.drawCircle(100, 100, 200);
+    graphics.interactive = true;
+    graphics.hitArea = new PIXI.Rectangle(-100, -100, 200, 200);
+    graphics.drag = false;
+    graphics.mousedown = graphics.touchstart = function(data) {
+	console.log(data);
+    };*/
 	
 };
+    
+Math.toRad = function(deg) {
+    return deg*(Math.PI/180);
+}
+Math.toDeg = function(rad) {
+    return rad*(180/Math.PI);
+}
