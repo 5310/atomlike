072afe4e5d8b90f94dc0c27aebce29371d269d9b
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2013-07-26 11:12:59 +0530

    Oops, forgot barrier component.

---
+++ game/components/barrier.js
@@ -0,0 +1,21 @@
+Crafty.c('Barrier', {
+	
+	init: function() {
+		
+		this.requires('2D, PIXI, PolyK');
+		
+		this.pixi_color = 0xdddddd;
+		this.pixi_setGraphics();
+		
+		this.polyk_setPolygon();
+		
+		this.bind( 'Move', function(data) {
+			if ( data._w != this.w || data._h != this.h ) {
+				this.pixi_setGraphics();
+				this.polyk_setPolygon();
+			}
+		} );
+		
+	}
+	
+});
