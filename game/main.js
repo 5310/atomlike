6155fef2c5a55cb0534ed6675175fc4c1c087371
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2013-07-26 10:38:22 +0530

    Barrier component wrapped.

--- game/main.js
+++ game/main.js
@@ -9,7 +9,6 @@ window.onload = function() {
     // Tests.
     
     f = Crafty.e("Flock");
-    
     for ( var c = 50; c > 0; c-- ) {
         var b = Crafty.e("2D, PIXI, Boid");
         b.attr({ w: 4, h: 4 });
@@ -18,21 +17,15 @@ window.onload = function() {
         f.flock_addBoid(b);
     }
     
-    e = Crafty.e("2D, PIXI, PolyK, Barrier").attr({x: 300, y: 240, w: 200, h: 20, rotation: 90});
+    e = Crafty.e("2D, PIXI, PolyK").attr({x: 300, y: 240, w: 200, h: 20, rotation: 90});
     e.pixi_setGraphics();
     e.pixi_setInteractive( true, true );
     e.pixi_setHitArea();
     e.polyk_setPolygon();
-    //console.log(e.polyk_polygon);
-    //console.log(e.polyk_transformedPolygon());
     
     d = Crafty.e("2D, PIXI, PolyK, Barrier").attr({x: 500, y: 240, w: 200, h: 20, rotation: 90});
-    d.pixi_setGraphics();
-    d.polyk_setPolygon();
     
     c = Crafty.e("2D, PIXI, PolyK, Barrier").attr({x: 500, y: 240, w: 100, h: 100, rotation: 45});
-    c.pixi_setGraphics();
-    c.polyk_setPolygon();
     
     /*graphics = new PIXI.Graphics();
     Crafty.pixi.container.addChild(graphics);
