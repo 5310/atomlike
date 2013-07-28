8e6104d7585b83cf36cde07bc9eca9723f445afc
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2013-07-28 19:38:10 +0530

    Css changes and centering for presentation.

--- game/systems/pixi.js
+++ game/systems/pixi.js
@@ -24,6 +24,18 @@ Crafty.extend({pixi: {
 		self.container = new PIXI.DisplayObjectContainer();
 		self.stage.addChild(self.container);
 		
-	}
+		Crafty.pixi.center();
+		
+	},
+	
+	center: function() {
+		
+		var container = document.getElementById("px-stage");
+		var canvas = Crafty.pixi.renderer.view;
+		canvas.style.marginLeft = (( container.offsetWidth - canvas.offsetWidth )/2)+"px";
+		canvas.style.marginTop = (( container.offsetHeight - canvas.offsetHeight )/2)+"px";
+		window.onresize = Crafty.pixi.center;
+		
+	},
 	
 }});
